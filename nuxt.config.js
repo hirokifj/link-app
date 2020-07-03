1f7b627bcc32bccb237ecc5146d42c38232b343b
hirokifj <hf.rowkey313@gmail.com>
2020-07-04 01:25:18 +0900

    dotenv利用に必要な設定を追記

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,6 +47,7 @@ export default {
    ** Nuxt.js dev-modules
    */
   buildModules: [
+    '@nuxtjs/dotenv',
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/stylelint-module
