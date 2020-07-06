e3bc108fb86e3ef218327d2cdbf2aac603689210
hirokifj <hf.rowkey313@gmail.com>
2020-07-06 19:55:32 +0900

    btnコンポーネント作成

---
+++ src/components/core/AppBtn.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="app-btn" @click="$emit('click')">
+    <slot></slot>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.app-btn {
+  display: inline-block;
+  padding: 10px 16px;
+  font-size: 14px;
+  font-weight: bold;
+  text-align: center;
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+  outline: none;
+
+  &.-big {
+    padding: 22px 20px;
+    font-size: 16px;
+  }
+
+  &.-full {
+    width: 100%;
+  }
+
+  &.-primary {
+    color: $color-white;
+    background-color: $color-primary;
+  }
+}
+</style>
