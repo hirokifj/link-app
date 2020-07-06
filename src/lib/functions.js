b9779e92993f985a32c94fcfe64e538b5e23c17e
hirokifj <hf.rowkey313@gmail.com>
2020-07-07 02:29:27 +0900

    firebaseのエラーメッセージを日本語で返す関数実装

---
+++ src/lib/functions.js
@@ -0,0 +1,23 @@
+export const getFirebaseErrMsgInJP = (code) => {
+  let errMsg = ''
+
+  switch (code) {
+    case 'auth/invalid-email':
+      errMsg = 'メールアドレスを正しく入力してください。'
+      break
+    case 'auth/email-already-in-use':
+      errMsg = 'このメールアドレスは既に利用されています。'
+      break
+    case 'auth/operation-not-allowed':
+      errMsg = '無効のユーザーアカウントです。'
+      break
+    case 'auth/weak-password':
+      errMsg = 'パスワードは6文字以上で入力してください。'
+      break
+    default:
+      errMsg = '処理に失敗しました。時間をおいて再度お試しください。'
+      break
+  }
+
+  return errMsg
+}
