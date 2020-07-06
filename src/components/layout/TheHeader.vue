a92bca7834048a778b588052e1a037473204c682
hirokifj <hf.rowkey313@gmail.com>
2020-07-06 20:34:10 +0900

    ヘッダーを仮実装

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="the-header"></header>
+</template>
+
+<style lang="scss" scoped>
+.the-header {
+  position: sticky;
+  top: 0;
+  width: 100%;
+  height: 60px;
+  background-color: $color-primary;
+}
+</style>
