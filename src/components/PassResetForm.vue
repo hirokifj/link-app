ce69411db0861899ee36f67b01641608d128f9b1
hirokifj <hf.rowkey313@gmail.com>
2020-07-07 22:22:50 +0900

    パスワードリセットページを作成

---
+++ src/components/PassResetForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="pass-reset-form">
+    <div class="control">
+      <AppLabel for="email" class="label" text="メールアドレス" />
+      <AppInput
+        id="email"
+        v-model="email"
+        type="text"
+        placeholder="メールアドレス"
+      />
+    </div>
+    <div class="submit">
+      <AppBtn class="-primary -big -full">
+        メール送信
+      </AppBtn>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppBtn from '../components/core/AppBtn'
+import AppLabel from '~/components/core/AppLabel'
+import AppInput from '~/components/core/AppInput'
+
+export default {
+  components: {
+    AppLabel,
+    AppInput,
+    AppBtn,
+  },
+  data() {
+    return {
+      email: '',
+    }
+  },
+  methods: {
+    submit() {
+      this.$emit('onSubmit', {
+        email: this.email,
+      })
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.pass-reset-form > .control {
+  margin-bottom: 42px;
+}
+
+.pass-reset-form > .control > .label {
+  margin-bottom: 8px;
+}
+</style>
---
+++ src/pages/reset/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="pass-reset-page-container">
+    <div class="form">
+      <div class="formwrapper">
+        <div class="form">
+          <PassResetForm />
+        </div>
+        <div class="text">
+          <p>
+            パスワード再設定メールを送信します。<br />登録したメールアドレスを入力してください。
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PassResetForm from '~/components/PassResetForm'
+
+export default {
+  layout: 'single',
+  components: {
+    PassResetForm,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.pass-reset-page-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+}
+
+.pass-reset-page-container > .form {
+  width: 90%;
+  max-width: 600px;
+}
+
+.pass-reset-page-container > .form > .formwrapper {
+  width: 100%;
+  padding: 60px 80px;
+  background-color: $color-white;
+  border-radius: 10px;
+}
+
+.pass-reset-page-container > .form > .formwrapper > .errmsg {
+  margin-bottom: 20px;
+  color: red;
+  text-align: center;
+}
+
+.pass-reset-page-container > .form > .formwrapper > .form {
+  width: 90%;
+  margin: 0 auto;
+  margin-bottom: 20px;
+}
+
+.pass-reset-page-container > .form > .formwrapper > .text {
+  width: 90%;
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
