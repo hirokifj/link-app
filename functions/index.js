8aa324398b1b99cada7ef5505ba2af59ea676142
hirokifj <hf.rowkey313@gmail.com>
2020-07-07 19:50:25 +0900

    ユーザ登録時、firestoreにドキュメントをバックグラウンドで追加する

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
-// const functions = require('firebase-functions')
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// ユーザー登録時、usersコレクションに当該ユーザーのドキュメントを作成する
+exports.onCreateUser = functions
+  .region('asia-northeast1')
+  .auth.user()
+  .onCreate(async (user) => {
+    const userRef = admin.firestore().doc(`users/${user.uid}`)
+
+    await admin.firestore().runTransaction(async (t) => {
+      // ドキュメントの存在確認
+      const userDoc = await t.get(userRef)
+      // ドキュメントが存在しない場合のみ処理する
+      if (!userDoc.exists) {
+        await t.set(userRef, {
+          displayId: 'aaa',
+          name: user.displayName,
+          profileImg: '',
+          type: '',
+          status: [],
+          copy: '',
+          comment: '',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        })
+      }
+    })
+  })
