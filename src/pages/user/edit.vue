750b9a82f3ba2ba9db867f0108123a5b6f408cdf
hirokifj <hf.rowkey313@gmail.com>
2020-07-08 20:19:36 +0900

    ユーザ情報編集ページ実装

---
+++ src/pages/user/edit.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="user-edit-page-container">
+    <div class="title">
+      <PageTitle>プロフィール編集</PageTitle>
+    </div>
+    <div class="form">
+      <UserEditForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import UserEditForm from '~/components/user/UserEditForm'
+
+export default {
+  components: {
+    UserEditForm,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.user-edit-page-container {
+  padding: 40px;
+
+  & > .title {
+    margin-bottom: 20px;
+  }
+
+  & > .form {
+    width: 60%;
+  }
+}
+</style>
