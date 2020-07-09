5f684da4ed83b9585e615013539985a629d33647
hirokifj <hf.rowkey313@gmail.com>
2020-07-09 23:32:25 +0900

    ユーザIDの更新処理実装

--- src/pages/user/id.vue
+++ src/pages/user/id.vue
@@ -4,13 +4,19 @@
       <PageTitle>ID設定</PageTitle>
     </div>
     <div class="form">
-      <UserSetIdForm :err-msg="errMsg" :user-id="displayId" />
+      <UserSetIdForm
+        :err-msg="errMsg"
+        :user-id="displayId"
+        @onSubmit="updateId"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import UserSetIdForm from '~/components/user/UserSetIdForm'
+import { isHankakuEisu, getFirebaseErrMsgInJP } from '~/lib/functions'
+import { displayIdExists, updateDisplayId } from '~/lib/api/user'
 
 export default {
   components: {
@@ -30,7 +36,45 @@ export default {
   data() {
     return {
       errMsg: '',
+      isProcessing: false,
     }
+  },
+  methods: {
+    async updateId(newDisplayId) {
+      // IDが変更されてない場合は何もしない。
+      if (this.displayId === newDisplayId) return
+
+      // バリデーション
+      // 必須チェック
+      if (newDisplayId === '') {
+        this.errMsg = 'IDを入力してください。'
+        return
+      }
+      // 半角英数字チェック
+      if (!isHankakuEisu(newDisplayId)) {
+        this.errMsg = '半角英数字で入力してください。'
+        return
+      }
+
+      try {
+        this.isProcessing = true
+
+        // 同一IDチェック
+        if (await displayIdExists(newDisplayId)) {
+          this.errMsg = 'このIDは既に利用されています。'
+          return
+        }
+
+        // ユーザIDの更新
+        await updateDisplayId(this.$currentUser.get().id, newDisplayId)
+        // 更新完了後、home画面へリダイレクト
+        this.$router.push('/home')
+      } catch (error) {
+        this.errMsg = getFirebaseErrMsgInJP(error.code)
+      } finally {
+        this.isProcessing = false
+      }
+    },
   },
 }
 </script>
