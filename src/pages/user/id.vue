497b7ef3a40754a1072cd8c9538e8ef89688c1e4
hirokifj <hf.rowkey313@gmail.com>
2020-07-09 22:11:04 +0900

    ユーザーID設定ページ実装

---
+++ src/pages/user/id.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="set-id-page-conatiner">
+    <div class="title">
+      <PageTitle>ID設定</PageTitle>
+    </div>
+    <div class="form">
+      <UserSetIdForm :err-msg="errMsg" />
+    </div>
+  </div>
+</template>
+
+<script>
+import UserSetIdForm from '~/components/user/UserSetIdForm'
+
+export default {
+  components: {
+    UserSetIdForm,
+  },
+  data() {
+    return {
+      errMsg: '',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.set-id-page-conatiner {
+  padding: 40px;
+
+  & > .title {
+    margin-bottom: 20px;
+  }
+
+  & > .form {
+    width: 60%;
+  }
+}
+</style>
